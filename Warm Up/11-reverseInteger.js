4bc0b220774020d5bcdd6928aa18d0d6e583fe2d
Lokendra Nath <nathdeveloper25@gmail.com>
2025-12-18 12:42:09 +0530

    reverse integer

---
+++ Warm Up/11-reverseInteger.js
@@ -0,0 +1,15 @@
+function reverse(x) {
+  let oNum = x;
+  x = Math.abs(x);
+  let rev = 0;
+  while (x > 0) {
+    let rem = x % 10;
+    rev = 10 * rev + rem;
+    x = Math.floor(x / 10);
+  }
+  let limit = Math.pow(2, 31);
+
+  if (rev < -limit || rev > limit) return 0;
+
+  return oNum < 0 ? -rev : rev;
+}
