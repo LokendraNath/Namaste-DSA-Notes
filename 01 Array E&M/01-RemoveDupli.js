55c8e16c3006cd620b9d174b94357b35c0daea5e
Lokendra Nath <nathdeveloper25@gmail.com>
2025-12-31 12:04:17 +0530

    remove duplicates

---
+++ 01 Array E&M/01-RemoveDupli.js
@@ -0,0 +1,13 @@
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+const removeDuplicates = function (nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x++;
+      nums[x] = nums[i];
+    }
+  }
+  return x + 1;
+};
+console.log(removeDuplicates(nums));
