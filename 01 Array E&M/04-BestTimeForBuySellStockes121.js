4497cac151e9601bca58c3107e366cbf0b4eb345
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-03 00:26:07 +0530

    121

---
+++ 01 Array E&M/04-BestTimeForBuySellStockes121.js
@@ -0,0 +1,18 @@
+let prices = [7, 1, 5, 3, 6, 4];
+
+const maxProfit = (prices) => {
+  let m = prices[0];
+  let mP = 0
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - m > mP) {
+      mP = prices[i] - m
+    }
+    if (prices[i] < m) {
+      m = prices[i]
+    }
+
+  }
+  return mP
+};
+console.log(maxProfit(prices));
