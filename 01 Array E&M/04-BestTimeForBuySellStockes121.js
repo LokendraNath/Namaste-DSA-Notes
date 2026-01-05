22d19ed153c9b9362b55a4d7c16689b269386e51
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-05 11:58:47 +0530

    clear code

--- 01 Array E&M/04-BestTimeForBuySellStockes121.js
+++ 01 Array E&M/04-BestTimeForBuySellStockes121.js
@@ -2,17 +2,16 @@ let prices = [7, 1, 5, 3, 6, 4];
 
 const maxProfit = (prices) => {
   let m = prices[0];
-  let mP = 0
+  let mP = 0;
 
   for (let i = 1; i < prices.length; i++) {
     if (prices[i] - m > mP) {
-      mP = prices[i] - m
+      mP = prices[i] - m;
     }
     if (prices[i] < m) {
-      m = prices[i]
+      m = prices[i];
     }
-
   }
-  return mP
+  return mP;
 };
 console.log(maxProfit(prices));
